b8c116fc22ef55909d9e4ade67a3b52196018d0f
Oscar Umana <oscarumana1021@gmail.com>
2023-02-08 22:04:13 -0500

    updated info

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -12,16 +12,28 @@ const projects = [
         githubUrl:"https://github.com/oscaru21/COMP229_WEBPORTFOLIO"
     },
     {
-        title: "Library Management System",
-        description: "Library management system developed during internship at Telus International using Spring Boot for the Backend and Vue.js for the Frontend. The system helped to optimize process for a local library.",
-        imagePath: "/images/png/project2.png",
-        githubUrl:"https://github.com/oscaru21/book-borrowing-app"
+        title: "Dessert ordering ecommerce",
+        description: "Dessert ordering system developed using Spring boot for the Backend and React.js for the Frontend, capable of creating and storing new database entries and identifying users with JWTs. This is an ongoing project.",
+        imagePath: "/images/png/project5.jpg",
+        githubUrl:"https://github.com/oscaru21/dessert-ordering"
+    },
+    {
+        title: "Survey Site",
+        description: "A full-stack survey application using the MEAN stack (MongoDB, Express, Angular, Node.js). Developed as a final project for COMP229",
+        imagePath: "/images/png/project4.jpg",
+        githubUrl:"https://github.com/oscaru21/survey_site"
     },
     {
         title: "Ticketing App",
         description: "Ticketing management system developed using Node.js for the Backend and React.js for the Frontend, capable of creating and storing new database entries and identifying users with JWTs. This app has helped me a lot to keep track of my current assignments.",
         imagePath: "/images/png/project3.png",
         githubUrl:"https://github.com/oscaru21/support-desk"
+    },
+    {
+        title: "Library Management System",
+        description: "Library management system developed during internship at Telus International using Spring Boot for the Backend and Vue.js for the Frontend. The system helped to optimize process for a local library.",
+        imagePath: "/images/png/project2.png",
+        githubUrl:"https://github.com/oscaru21/book-borrowing-app"
     },
 ]
 
